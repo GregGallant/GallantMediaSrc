15e01725d0b4232fe778db87f13d970c0ce2ca4f
Greg Gallant <greg@privco.com>
2016-08-30 18:12:31 -0400

    Vue resource added

--- src/main/resources/static/gmedia.js
+++ src/main/resources/static/gmedia.js
@@ -1,7 +1,35 @@
-
-new Vue({
+// npm vue, vue-resource
+var vm = new Vue({
 	el: '#app',
 	data: {
-		message: 'A Loner, Pee Wee.  A Rebel.'
+		message: 'Second',
+		message2: 'Third'
+	},
+	methods: {
+		setMessage: function (message) {
+			this.message = message;	
+		},
+		getApiData: function (url, data) {
+            this.message = 'loading...';
+			this.$http.get(url,data)
+				.success(function(result) {
+					console.log('API Success');	
+					this.setMessage('API is success');
+				})
+				.error(function() {
+					console.log('API Fail.');
+					this.setMessage('API opposite of working');	
+				});
+		}
 	}
-})
+});
+
+vm.getApiData('/photography');
+
+new Vue({
+	el: '#secc',
+	data: {
+		seccMsg: 'First Section',
+		seccMsg2: 'Second Section'
+	}
+});
